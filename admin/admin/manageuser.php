b6f0f0c25e65de8822d1f2d2b25f6717033877eb
Alif720 <m.alifaimansmd@gmail.com>
2023-05-26 17:31:25 +0800

    Update manageuser.php

--- admin/admin/manageuser.php
+++ admin/admin/manageuser.php
@@ -6,7 +6,12 @@ if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
     $user_id=$_GET['user_id'];
 
     /*this is delete query*/
-    mysqli_query($con,"delete from user_info where user_id='$user_id'")or die("query is incorrect...");
+    $delete_query = mysqli_query($con,"delete from user_info where user_id='$user_id'")or die("query is incorrect...");
+
+    if ($delete_query) {
+        $_SESSION['delete_success'] = "User deleted successfully.";
+    }
+  
 }
 
 include "sidenav.php";
@@ -21,6 +26,12 @@ include "topheader.php";
                     <h4 class="card-title">Manage User</h4>
                 </div>
                 <div class="card-body">
+                    <?php if(isset($_SESSION['delete_success'])): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo $_SESSION['delete_success']; ?>
+                        </div>
+                        <?php unset($_SESSION['delete_success']); ?>
+                    <?php endif; ?>
                     <div class="table-responsive ps">
                         <table class="table tablesorter table-hover" id="">
                             <thead class="text-primary">
@@ -63,6 +74,7 @@ include "topheader.php";
                         <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
